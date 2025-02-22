b5a4c8e60f438a1e9f720d65c8ab9b3a02ac73ab
Aayush <ayushkarkee6@gmail.com>
2025-02-22 22:44:49 +0545

    created feature card

---
+++ src/components/landing/FeatureCard.tsx
@@ -0,0 +1,24 @@
+import { DivideIcon as LucideIcon } from "lucide-react";
+import { Card, CardDescription, CardHeader, CardTitle } from "../ui/card";
+
+interface FeatureCardProps {
+  icon: LucideIcon;
+  title: string;
+  description: string;
+}
+
+export default function FeatureCard({
+  icon: Icon,
+  title,
+  description,
+}: FeatureCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <Icon className="h-10 w-10 mb-2" />
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+    </Card>
+  );
+}
