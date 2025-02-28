4acef498a175842be506d9cf8bc0a5c805bc452c
Aayush <ayushkarkee6@gmail.com>
2025-03-01 00:53:59 +0545

    update :: made the login form responsive

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,7 +1,18 @@
 import makeup from "../../assets/makeup kit.jpg";
 import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { loginFormData, loginFormSchema } from "../../lib/validator";
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Link } from "react-router-dom";
 
 const LoginForm = () => {
   const form = useForm<loginFormData>({
@@ -12,11 +23,15 @@ const LoginForm = () => {
     },
     mode: "onChange",
   });
+
+  const handleSubmit = (values: loginFormData) => {
+    console.log(values);
+  };
   return (
     <div className="flex items-center justify-center w-full min-h-screen">
-      <div className="flex items-center w-full max-w-screen-lg bg-gray-700 rounded-lg">
+      <div className="flex items-center w-full max-w-sm mx-4 bg-gray-300 rounded-lg shadow-sm md:max-w-screen-lg">
         {/*Left side*/}
-        <div className="w-1/2">
+        <div className="w-1/2 max-md:hidden">
           <img
             src={makeup}
             alt="makeup image"
@@ -27,7 +42,64 @@ const LoginForm = () => {
         </div>
 
         {/*Right side*/}
-        <div className="w-1/2"></div>
+        <div className="w-full px-6 py-10 md:w-1/2">
+          <div className="text-center">
+            <h2 className="text-xl font-bold text-gray-800">
+              Login to GlowGuide
+            </h2>
+            <p className="text-gray-600 text-md">
+              Sign in to access your personalized skincare routine and expert
+              advice. ✨
+            </p>
+          </div>
+
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(handleSubmit)}
+              className="flex flex-col w-full mt-4 space-y-4"
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="example@gmail.com" {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter your Password" {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <p className="mt-4 text-sm text-blue-600 cursor-pointer">
+                Forgot Password?
+              </p>
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+          <p className="mt-5 text-center">
+            Don't have an account?
+            <Link to={"/register"}>
+              <span className="text-blue-600 cursor-pointer"> Signup</span>
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
