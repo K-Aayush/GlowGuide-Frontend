2965d97d08e976b91eba269d85abc2e35c6b205c
Aayush <ayushkarkee6@gmail.com>
2025-03-01 00:24:16 +0545

    using useform hook with zod

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,6 +1,17 @@
 import makeup from "../../assets/makeup kit.jpg";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers";
+import { loginFormData, loginFormSchema } from "../../lib/validator";
 
 const LoginForm = () => {
+  const form = useForm<loginFormData>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onChange",
+  });
   return (
     <div className="flex items-center justify-center w-full min-h-screen">
       <div className="flex items-center w-full max-w-screen-lg bg-gray-700 rounded-lg">
@@ -16,9 +27,7 @@ const LoginForm = () => {
         </div>
 
         {/*Right side*/}
-        <div className="w-1/2">
-          
-        </div>
+        <div className="w-1/2"></div>
       </div>
     </div>
   );
