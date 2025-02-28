d600f396369ac5f1d8cafbd1a445c4ec5eef9861
Aayush <ayushkarkee6@gmail.com>
2025-03-01 00:58:04 +0545

    created zodschema for register

--- src/lib/validator.ts
+++ src/lib/validator.ts
@@ -1,8 +1,34 @@
 import { z } from "zod";
 
 export type loginFormData = z.infer<typeof loginFormSchema>;
+export type registerFormData = z.infer<typeof registerFormSchema>;
 
 export const loginFormSchema = z.object({
   email: z.string({ required_error: "Email is required" }),
   password: z.string({ required_error: "Password is required" }),
+});
+
+export const registerFormSchema = z.object({
+  email: z
+    .string({ required_error: "Email is required" })
+    .email("Invalid email formate"),
+  password: z
+    .string({ required_error: "Password is required" })
+    .min(3, "Must be 3 or more characters long")
+    .max(20, "Must be less than 20 characters")
+    .refine(
+      (password) => /[A-Z]/.test(password),
+      "Must contain one capital letter"
+    )
+    .refine(
+      (password) => /[a-z]/.test(password),
+      "Must contain one small letter"
+    )
+    .refine((password) => /[0-9]/.test(password), "Must contain one number")
+    .refine(
+      (password) => /[!@#$%^&*]/.test(password),
+      "Must contain one special character"
+    ),
+  name: z.string().min(3, "Name is required"),
+  role: z.enum(["USER", "DERMATOLOGISTS"]).default("USER"),
 });
