a35787c5c7fc790767a386ecedc5cab641d8e30b
Aayush <ayushkarkee6@gmail.com>
2025-03-01 00:09:18 +0545

    creating login form

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,5 +1,25 @@
+import makeup from "../../assets/makeup kit.jpg";
+
 const LoginForm = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex items-center justify-center w-full min-h-screen">
+      <div className="flex items-center w-full max-w-screen-lg bg-gray-700 rounded-lg">
+        {/*Left side*/}
+        <div className="w-1/2">
+          <img
+            src={makeup}
+            alt="makeup image"
+            loading="lazy"
+            decoding="async"
+            className="object-contain object-center w-full h-full rounded-l-lg"
+          />
+        </div>
+
+        {/*Right side*/}
+        <div className="w-1/2">Right</div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginForm;
