37c295e92a6b8aff7e54b64bcdf0e0d46a9cc328
Aayush <ayushkarkee6@gmail.com>
2025-07-30 11:18:34 +0545

    updated service

--- src/api/services/adminService.ts
+++ src/api/services/adminService.ts
@@ -11,6 +11,23 @@ const adminService = {
     const { data } = await apiClient.get("/api/admin/users");
     return data.users;
   },
+  getPendingDermatologists: async (): Promise<UserData[]> => {
+    const { data } = await apiClient.get("/api/admin/pending-dermatologists");
+    return data.dermatologists;
+  },
+
+  approveDermatologist: async (
+    id: string,
+    status: "APPROVED" | "REJECTED"
+  ): Promise<UserData> => {
+    const { data } = await apiClient.patch(
+      `/api/admin/dermatologists/${id}/approve`,
+      {
+        status,
+      }
+    );
+    return data.user;
+  },
 
   deleteUser: async (id: string): Promise<void> => {
     await apiClient.delete(`/api/admin/users/${id}`);
